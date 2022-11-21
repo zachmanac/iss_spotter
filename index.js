2facd5b4cae20f4060ebb70732319802140bedca
zachmanac <zachstephenson94@gmail.com>
2022-11-21 15:12:31 -0800

    added fetchmyip function and fetchcoordsbyip function

---
+++ index.js
@@ -0,0 +1,20 @@
+const { fetchCoordsByIP, fetchMyIP } = require("./iss");
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+// temp code to make sure function works as intended.
+
+// fetchCoordsByIP('172.103.209.59', (error, coordinates) => {
+//     if (error) {
+//       console.log("It didn't work!" , error);
+//       return;
+//     }
+  
+//     console.log('It worked! Returned latitude and longitude' , coordinates);
+// });
---
+++ iss.js
@@ -0,0 +1,68 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (response.statusCode === 200) {
+      callback(null, data.ip);
+    }
+    
+  });
+  
+};
+
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    // check if "success" is true or not
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+
+    // const lat = parsedBody.latitude;
+    // const long = parsedBody.longitude;
+    // const latLong = { latitude: `${lat}`, longitude: `${long}` };
+    
+    // callback(null, latLong);
+  
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
